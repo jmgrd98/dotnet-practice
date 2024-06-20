28222c6de7805337969f49fc722ac4d1f929b413
joaodantasdev <jmgrd98@gmail.com>
2024-06-20 13:37:06 -0300

    commit

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Digite seu nome: ");
+string name = Console.ReadLine();
+
+Console.WriteLine("Digite o ano do seu nascimento: ");
+int year = int.Parse(Console.ReadLine());
+int age = DateTime.Now.Year - year;
+
+Console.WriteLine($"Olá {name}, você possui {age} anos!");
